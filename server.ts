bfcf323384ab418a7aa90667f22ed806c0f28601
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-07-08 17:09:21 +0200

    Added simple configuration for socket.io

--- server.ts
+++ server.ts
@@ -1,11 +1,31 @@
 import express from "express";
-const app = express();
-const PORT = 8080;
+import { Server } from "socket.io";
+import cors from "cors";
+import http from "http";
 
+const PORT = 8090 || process.env.PORT;
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Server is running");
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("New user connected :)");
+  socket.emit("message", "Hello new user");
+  socket.on("disconnect", () => {
+    io.emit("message", "User disconnected");
+  });
 });
