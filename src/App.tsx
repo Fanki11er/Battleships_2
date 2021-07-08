f453685061ab14d034873bd52c80829ad39cd618
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-07-08 17:10:47 +0200

    Added simple configuration for socket.io

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+//import axios from "axios";
+import { io, Socket } from "socket.io-client";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div>
-     BattleShip2
-    </div>
-  );
+  const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  /*useEffect(() => {
+    axios.get("http://localhost:8090").then(({ data }) => {
+      console.log(data);
+    });
+  }, []);*/
+  useEffect(() => {
+    setSocket(io("http://localhost:8090"));
+  }, []);
+
+  useEffect(() => {
+    socket &&
+      socket.on("message", (message) => {
+        console.log(message);
+      });
+  }, [socket]);
+  return <div>BattleShip2</div>;
 }
 
 export default App;
