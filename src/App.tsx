b83650caca06a6db6773026f3dcbad56922794f4
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-07-09 12:40:57 +0200

    Added connection to server and getting rooms list by socket.io

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,28 @@ import { useState } from 'react';
 
 function App() {
   const [socket, setSocket] = useState<Socket | undefined>(undefined);
+  const [roomsList, setRoomsList] = useState<[]>([]);
+
   /*useEffect(() => {
-    axios.get("http://localhost:8090").then(({ data }) => {
+    axios.get("http://localhost:8090").then(({ data }) => { 
       console.log(data);
-    });
+    }); 
   }, []);*/
   useEffect(() => {
     setSocket(io('http://localhost:8090'));
   }, []);
 
   useEffect(() => {
-    socket &&
-      socket.on('message', (message) => {
-        console.log(message);
-      });
+    socket?.emit('joinRoom', { userName: 'Krzysiek', roomName: 'TestRoom' });
   }, [socket]);
+
+  useEffect(() => {
+    socket?.on('RoomsList', (rooms: []) => {
+      setRoomsList(rooms);
+    });
+    console.log(roomsList);
+  }, [socket, roomsList]);
+
   return <div>BattleShip2</div>;
 }
 
