83bc36f19fba47e52b5e066096c072278330f93f
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-07-17 13:23:43 +0200

    Added simple info about user and oponent status in preparing Page

--- client/src/Views/PreparingPage/PreparingPage.tsx
+++ client/src/Views/PreparingPage/PreparingPage.tsx
@@ -8,25 +8,67 @@ import { useLocation } from 'react-router-dom';
 import { useContext } from 'react';
 import { SocketContext } from '../../providers/socketProvider';
 import { useEffect } from 'react';
+import { useState } from 'react';
+import { User } from '../../Types/types';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const BoardWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+`;
+
+const StatusWrapper = styled.div`
+  width: 100%;
+  height: 150px;
+  display: flex;
+`;
+
+const UserInfo = styled.div`
+  display: flex;
+  width: 400px;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 const PreparingPage = () => {
   const { state } = useLocation();
   const { socket } = useContext(SocketContext);
+  const [users, setUsers] = useState<User[]>([]);
   const test = state as any;
   const { roomName } = test;
 
   useEffect(() => {
     socket?.emit('usersJoinTheRoom', roomName);
-    socket?.on('usersStatusInRoom', (users) => {});
+    socket?.on('usersStatusInRoom', (users) => {
+      setUsers(users);
+    });
   }, [socket, roomName]);
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <ShipsProvider>
-        <Board></Board>
-        <ShipsList />
-      </ShipsProvider>
-    </DndProvider>
+    <Wrapper>
+      <StatusWrapper>
+        <UserInfo>
+          <div>{`Your status: ${users[0] && users[0].status}`}</div>
+          <div>{`Opponent status: ${users.length > 1 ? users[1].status : 'No opponent'}`}</div>
+        </UserInfo>
+      </StatusWrapper>
+      <BoardWrapper>
+        <DndProvider backend={HTML5Backend}>
+          <ShipsProvider>
+            <Board></Board>
+            <ShipsList />
+          </ShipsProvider>
+        </DndProvider>
+      </BoardWrapper>
+    </Wrapper>
   );
 };
 
