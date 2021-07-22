5084cd675c9ea45bbbb0ec79f470ef66a09589a9
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-07-22 21:36:44 +0200

    Added rest of test for Room component

--- client/src/components/Organisms/Room/Room.test.tsx
+++ client/src/components/Organisms/Room/Room.test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react';
-import renderWithProviders from '../../../Helpers/RenderWithProviders';
-import { Default, TwoUsersWithComputer } from './Room.stories';
+import renderWithProviders from '../../../Helpers/renderWithProviders';
+import { Default, TwoUsersWithComputer, WithOneUser } from './Room.stories';
 
 describe('Room', () => {
   describe('when get empty users array', () => {
@@ -10,25 +10,57 @@ describe('Room', () => {
       expect(slots.length).toBe(2);
     });
 
-    /*it('Should have active button', () => {
+    it('Should have active button', () => {
       renderWithProviders(<Default {...Default.args} />);
       const button = screen.getByRole('button');
       expect(button).toBeInTheDocument();
-      expect(button.style.getPropertyValue('user-select')).toBe('');
-    });*/
+      expect(button).toHaveStyle('user-select: initial');
+    });
   });
 
   describe('when has two users already', () => {
-    it('Should render two empty slots', () => {
+    it('Should render zero empty slots', () => {
       renderWithProviders(<TwoUsersWithComputer {...TwoUsersWithComputer.args} />);
       const slots = screen.queryAllByText(/Empty/i);
       expect(slots.length).toBe(0);
     });
-    /*it('Should have not active button', async () => {
-      renderWithProviders(<Default {...Default.args} />);
-      const button = await screen.findByRole('button');
+    it('Should have not active button', () => {
+      renderWithProviders(<TwoUsersWithComputer {...TwoUsersWithComputer.args} />);
+      const button = screen.getByRole('button');
       expect(button).toBeInTheDocument();
-      expect(button)
-    });*/
+      expect(button).toHaveStyle('user-select: none');
+    });
+
+    it('Should have two user names', () => {
+      renderWithProviders(<TwoUsersWithComputer {...TwoUsersWithComputer.args} />);
+      const firstElement = screen.getByText(/AI/i);
+      const secondElement = screen.getByText(/Krzysztof/i);
+      expect(firstElement).toBeInTheDocument();
+      expect(secondElement).toBeInTheDocument();
+    });
+    it('Should have one computer user and one real user', () => {
+      renderWithProviders(<TwoUsersWithComputer {...TwoUsersWithComputer.args} />);
+      const user = screen.getByText(/person/i);
+      const computer = screen.getByText(/computer/i);
+      expect(user).toBeInTheDocument();
+      expect(computer).toBeInTheDocument();
+    });
+  });
+
+  describe('When it has one user', () => {
+    it('Should have active button', () => {
+      renderWithProviders(<WithOneUser {...WithOneUser.args} />);
+      const button = screen.getByRole('button');
+      expect(button).toBeInTheDocument();
+      expect(button).toHaveStyle('user-select: initial');
+    });
+
+    it('Should have one  user name and one empty slot', () => {
+      renderWithProviders(<WithOneUser {...WithOneUser.args} />);
+      const user = screen.getByText(/Krzysztof/i);
+      const emptySlot = screen.getByText(/EMPTY/i);
+      expect(user).toBeInTheDocument();
+      expect(emptySlot).toBeInTheDocument();
+    });
   });
 });
