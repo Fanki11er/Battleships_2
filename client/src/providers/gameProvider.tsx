fcd71273df081608597c9201645175c1d140522f
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-08-01 13:20:46 +0200

    Created GameProvider and UserGameBoard

---
+++ client/src/providers/gameProvider.tsx
@@ -0,0 +1,26 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { Shot } from '../Types/types';
+import { SocketContext } from './socketProvider';
+
+export const GameContext = createContext({
+  shots: [] as Shot[],
+});
+
+const GameProvider = (props: React.PropsWithChildren<React.ReactNode>) => {
+  const [shots, setShots] = useState<Shot[]>([]);
+  const { socket } = useContext(SocketContext);
+
+  useEffect(() => {
+    socket?.on('shot', (shots: Shot[]) => {
+      setShots(shots);
+    });
+  });
+
+  const gameContext = {
+    shots,
+  };
+
+  return <GameContext.Provider value={gameContext}></GameContext.Provider>;
+};
+
+export default GameProvider;
