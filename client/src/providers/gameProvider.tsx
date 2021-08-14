46edf9563d63fc1da85ecf7e01b52884973ce87a
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-08-14 11:32:14 +0200

    Added leogic for creating shipsLeftList and added handler for shipSunk

--- client/src/providers/gameProvider.tsx
+++ client/src/providers/gameProvider.tsx
@@ -1,15 +1,20 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { Coordinates } from '../Class/BattleShip';
-import { Result, Shot, ShotResult } from '../Types/types';
+import { ShipListCreator, shipsList } from '../Data/shipsList';
+import { Result, shipsLeftListElement, Shot, ShotResult } from '../Types/types';
 import { SocketContext } from './socketProvider';
+import { v4 as uuid } from 'uuid';
 
 export const GameContext = createContext({
   myShots: [] as ShotResult[],
   opponentShots: [] as ShotResult[],
+  myShipsList: [] as shipsLeftListElement[],
+  opponentShipsList: [] as shipsLeftListElement[],
   handleShot: (coordinates: Coordinates) => {},
   isMyTurn: false,
   isGameStarted: false,
   checkIfItIsMyTurn: (currentPlayer: string) => {},
+  handleSunkShip: (userId: string, size: number) => {},
   winner: '',
 });
 
@@ -17,6 +22,8 @@ const GameProvider = (props: React.PropsWithChildren<React.ReactNode>) => {
   const { children } = props;
   const [myShots, setShots] = useState<ShotResult[]>([]);
   const [opponentShots, setOpponentShots] = useState<ShotResult[]>([]);
+  const [myShipsList, setMyShipsList] = useState<shipsLeftListElement[]>([]);
+  const [opponentShipsList, setOpponentShipsList] = useState<shipsLeftListElement[]>([]);
   const { socket } = useContext(SocketContext);
   const [isGameStarted, setGameStarted] = useState<boolean>(false);
   const [isMyTurn, setIsMyTurn] = useState(false);
@@ -59,6 +66,7 @@ const GameProvider = (props: React.PropsWithChildren<React.ReactNode>) => {
       setOpponentShots([]);
       setIsMyTurn(false);
       setWinner('');
+      createShipsLists(shipsList);
     });
     return () => {
       socket?.off('GameEnded');
@@ -68,11 +76,50 @@ const GameProvider = (props: React.PropsWithChildren<React.ReactNode>) => {
     socket?.emit('shot', { coordinates, userId: socket.id } as Shot);
   };
 
+  useEffect(() => {
+    createShipsLists(shipsList);
+  }, []);
+
   const checkIfItIsMyTurn = (currentTurnId: string) => {
     if (currentTurnId === socket?.id) {
       setIsMyTurn(true);
     } else {
       setIsMyTurn(false);
+    }
+  };
+
+  const createShipsLists = (shipsList: ShipListCreator[]) => {
+    const myList: shipsLeftListElement[] = [];
+    const opponentsList: shipsLeftListElement[] = [];
+    shipsList.forEach(({ size, quantity }) => {
+      for (let i = 0; i < quantity; i++) {
+        myList.push({ size, isSunk: false, id: uuid() } as shipsLeftListElement);
+        opponentsList.push({ size, isSunk: false, id: uuid() } as shipsLeftListElement);
+      }
+    });
+    setMyShipsList(myList);
+    setOpponentShipsList(opponentsList);
+  };
+
+  const handleSunkShip = (userId: string, size: number) => {
+    if (userId !== socket?.id) {
+      const opponentList = [...opponentShipsList];
+      for (let i = 0; i < opponentList.length; i++) {
+        if (!opponentList[i].isSunk && opponentList[i].size === size) {
+          opponentList[i].isSunk = true;
+          break;
+        }
+      }
+      setOpponentShipsList(opponentList);
+    } else {
+      const myList = [...myShipsList];
+      for (let i = 0; i < myList.length; i++) {
+        if (!myList[i].isSunk && myList[i].size === size) {
+          myList[i].isSunk = true;
+          break;
+        }
+      }
+      setMyShipsList(myList);
     }
   };
 
@@ -83,7 +130,10 @@ const GameProvider = (props: React.PropsWithChildren<React.ReactNode>) => {
     isMyTurn,
     isGameStarted,
     checkIfItIsMyTurn,
+    handleSunkShip,
     winner,
+    myShipsList,
+    opponentShipsList,
   };
 
   return <GameContext.Provider value={gameContext}>{children}</GameContext.Provider>;
