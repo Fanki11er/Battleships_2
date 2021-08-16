90ff982326dfa3689ec09632eab4babd88608bba
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2021-08-16 21:32:04 +0200

    Added background to landingPage

--- client/src/Views/LandingPage/LandingPage.tsx
+++ client/src/Views/LandingPage/LandingPage.tsx
@@ -1,17 +1,40 @@
-//import styled from 'styled-components';
+import styled from 'styled-components';
 
 import { useContext } from 'react';
 import { Redirect } from 'react-router-dom';
 import UserNameForm from '../../components/Organisms/UserNameForm/UserNameForm';
 import { UserContext } from '../../providers/userProvider';
 import { routes } from '../../router/routes';
+import backgroundImage from '../../assets/Images/landing-page-background.svg';
+import { StyledProps } from '../../assets/styles/theme';
+import Footer from '../../components/Molecules/Footer/Footer';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  height: 100vh;
+  background: url(${backgroundImage});
+  background-color: ${(props: StyledProps) => props.theme.colors.darkBlue};
+  background-size: cover;
+  background-blend-mode: luminosity;
+  background-position: 0 40%;
+  display: grid;
+  grid-template-rows: 200px 1fr 100px;
+`;
 
 const LandingPage = () => {
   const { roomsList } = routes;
   const { userName } = useContext(UserContext);
   if (userName) return <Redirect to={{ pathname: roomsList }}></Redirect>;
 
-  return <UserNameForm />;
+  return (
+    <Wrapper>
+      <p></p>
+      <p></p>
+      <Footer />
+    </Wrapper>
+  );
 };
 
 export default LandingPage;
+/*<UserNameForm />*/
