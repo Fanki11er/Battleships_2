9bafae5ed3317ce7f8639d44a464c44ebdc59d88
Fanki11er <30950091+Fanki11er@users.noreply.github.com>
2022-06-17 22:29:41 +0200

    Added ship sizes to shipsLeftList

--- client/src/providers/socketProvider.tsx
+++ client/src/providers/socketProvider.tsx
@@ -8,10 +8,10 @@ export const SocketContext = createContext({
 const SocketProvider = (props: React.PropsWithChildren<React.ReactNode>) => {
   const { children } = props;
 
-  //const URL = 'http://192.168.10.101:8090';
+  const URL = 'http://192.168.10.101:8090';
   //const URL = 'http://192.168.1.129:8090';
 
-  const URL = 'https://kdz-battleships-server.herokuapp.com/' || 'https//localhost:8090';
+  //const URL = 'https://kdz-battleships-server.herokuapp.com/' || 'https//localhost:8090';
 
   const [socket, setSocket] = useState<Socket | undefined>(undefined);
   useEffect(() => {
